dd59227ca97f1310fe3f1acf2f5967124948d38c
Bob <bobnelson.bn@gmail.com>
2018-09-05 22:01:12 -0400

    portfolio first upload

---
+++ Portfolio/projects/phpmedialibraryproject/inc/footer.php
@@ -0,0 +1,16 @@
+</div><!-- end content -->
+
+	</div>
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
